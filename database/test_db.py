e9b2e410e64ca880fdd222cd44a4155a5faea961
callistoitsolutions <124126575+callistoitsolutions@users.noreply.github.com>
2026-02-20 12:37:03 +0530

    Update test_db.py

--- database/test_db.py
+++ database/test_db.py
@@ -2,6 +2,7 @@ from db import engine
 
 try:
     with engine.connect() as conn:
-        print("✅ Connected to Railway MySQL successfully")
+        print("✅ Database connected successfully")
 except Exception as e:
-    print("❌ Connection failed:", e)
+    print("❌ Connection failed")
+    print(e)
