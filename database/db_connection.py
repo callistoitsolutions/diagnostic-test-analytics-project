2edc8d433b8475aa0313f37ee0b77198efa0c7c1
callistoitsolutions <124126575+callistoitsolutions@users.noreply.github.com>
2026-02-20 12:21:35 +0530

    Update db_connection.py

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
 from urllib.parse import quote_plus
 
-DB_USER = "root"
-DB_PASSWORD = quote_plus("palash@123")  # IMPORTANT
-DB_HOST = "localhost"
-DB_PORT = "3306"
-DB_NAME = "Diagnostic_Analytics"
+DB_USER = "MYSQL_USER_FROM_RAILWAY"
+DB_PASSWORD = quote_plus("MYSQL_PASSWORD_FROM_RAILWAY")
+DB_HOST = "MYSQL_HOST_FROM_RAILWAY"
+DB_PORT = "MYSQL_PORT_FROM_RAILWAY"
+DB_NAME = "MYSQL_DATABASE_FROM_RAILWAY"
 
 engine = create_engine(
     f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
+
+print("✅ Database engine created successfully")
